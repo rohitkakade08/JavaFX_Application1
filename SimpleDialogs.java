883753b88ac49ac0de7c6eefe0094b43e1653454
Rohit Kakade <92684754+rohitkakade08@users.noreply.github.com>
2023-10-16 15:02:20 +0530

    Add files via upload

---
+++ SimpleDialogs.java
@@ -0,0 +1,275 @@
+
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.geometry.Insets;
+import javafx.event.ActionEvent;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class SimpleDialogs {
+    
+    public static boolean custom(Node content, String title) {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle(title);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL, ButtonType.OK);
+        dialog.getDialogPane().setContent(content);
+        Optional<ButtonType> result = dialog.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
+    }
+    
+    public static boolean vetoableInput(
+            Node inputNode, String headerText, Supplier<String> testForErrorString) {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Input is Requested");
+        dialog.setHeaderText(headerText);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL, ButtonType.OK);
+        dialog.getDialogPane().setContent(inputNode);
+        
+        Button okButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
+        okButton.addEventFilter(ActionEvent.ACTION, e -> {
+             if (testForErrorString.get() != null) {
+                 e.consume();
+                 message("Input is incorrect: " + testForErrorString.get());
+             }
+        });
+        
+        Optional<ButtonType> result = dialog.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
+    }
+    
+    public static void message(String text) {
+        message(text,"Message");
+    }
+
+    public static void message(String text, String title) {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setGraphic(null);
+        dialog.setTitle(title);
+        dialog.getDialogPane().setContent(makeText(text));
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().setPadding(new Insets(10,10,0,10));
+        dialog.showAndWait();
+    }
+    
+    public static String prompt(String promptText) {
+        return prompt(promptText,"Request for Input",null);
+    }
+    
+    public static String prompt(String promptText, String title) {
+        return prompt(promptText,title,null);
+    }
+    
+    public static String prompt(String promptText, String title, String defaultValue) {
+        TextInputDialog dialog = new TextInputDialog(defaultValue);
+        dialog.setTitle(title);
+        dialog.setGraphic(null);
+        dialog.setHeaderText(promptText);
+        Optional<String> reply = dialog.showAndWait();
+        if (reply.isPresent())
+            return reply.get();
+        else
+            return null;
+    }
+    
+    public static String confirm(String promptText) {
+        return confirm(promptText, "Confirmation needed (or cancel)");
+    }
+    
+    public static String confirm(String message, String title) {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle(title);
+        dialog.setHeaderText(null);
+        dialog.setContentText(message);
+        dialog.getDialogPane().getButtonTypes().addAll(
+                ButtonType.CANCEL, ButtonType.NO, ButtonType.YES);
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.YES)
+            return "yes";
+        else if (result.isPresent() && result.get() == ButtonType.NO)
+            return "no";
+        else
+            return "cancel";
+    }
+    
+    
+    public static Color colorChooser( Color initialColor ) {
+        return colorChooser(initialColor, "Select a Color");
+    }
+
+    public static Color colorChooser( Color initialColor, String headerText ) {
+        ColorChooserPane chooser = new ColorChooserPane(initialColor);
+        
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Color Picker");
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL, ButtonType.OK);
+        dialog.getDialogPane().setContent(chooser);
+        dialog.setHeaderText(headerText);
+        
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK )
+            return chooser.getColor();
+        else
+            return null;
+    }
+    
+    
+    
+    private final static int TEXT_WIDTH = 450;
+    
+    private static Text makeText(String str) {
+        Text text = new Text(str);
+        text.setWrappingWidth(TEXT_WIDTH);
+        text.setFont(Font.font(Font.getDefault().getSize()*1.2));
+        return text;
+    }
+        
+    
+    private static class ColorChooserPane extends GridPane {
+
+        private Slider hueSlider, brightnessSlider, saturationSlider,  
+                            redSlider, greenSlider, blueSlider;
+
+        private Label hueLabel, brightnessLabel, saturationLabel,  
+                            redLabel, greenLabel, blueLabel;
+
+        private Pane colorPatch;  
+
+        private Color currentColor;
+        
+        public ColorChooserPane(Color initialColor) {
+
+
+            hueSlider = new Slider(0,360,0);
+            saturationSlider = new Slider(0,1,1);
+            brightnessSlider = new Slider(0,1,1);
+            redSlider = new Slider(0,1,1);
+            greenSlider = new Slider(0,1,0);
+            blueSlider = new Slider(0,1,0);
+            
+            
+            hueSlider.valueProperty().addListener( e -> newColor(hueSlider) );
+            saturationSlider.valueProperty().addListener( e -> newColor(saturationSlider) );
+            brightnessSlider.valueProperty().addListener( e -> newColor(brightnessSlider) );
+            redSlider.valueProperty().addListener( e -> newColor(redSlider) );
+            greenSlider.valueProperty().addListener( e -> newColor(greenSlider) );
+            blueSlider.valueProperty().addListener( e -> newColor(blueSlider) );
+
+
+            hueLabel = makeText(String.format(" Hue = %1.3f", 0.0));
+            saturationLabel = makeText(String.format(" Saturation = %1.3f", 1.0));
+            brightnessLabel = makeText(String.format(" Brightness = %1.3f", 1.0));
+            redLabel = makeText(String.format(" Red = %1.3f", 1.0));
+            greenLabel = makeText(String.format(" Green = %1.3f", 0.0));
+            blueLabel = makeText(String.format(" Blue = %1.3f", 0.0));
+
+            
+            colorPatch = new Pane();
+            colorPatch.setStyle("-fx-background-color:red; -fx-border-color:black; -fx-border-width:2px");
+            
+
+            GridPane root = this;
+            ColumnConstraints c1 = new ColumnConstraints();
+            c1.setPercentWidth(33);
+            ColumnConstraints c2 = new ColumnConstraints();
+            c2.setPercentWidth(34);
+            ColumnConstraints c3 = new ColumnConstraints();
+            c3.setPercentWidth(33);
+            root.getColumnConstraints().addAll(c1, c2, c3);
+            
+            root.add(hueSlider, 0, 0);
+            root.add(saturationSlider, 0, 1);
+            root.add(brightnessSlider, 0, 2);
+            root.add(redSlider, 0, 3);
+            root.add(greenSlider, 0, 4);
+            root.add(blueSlider, 0, 5);
+            root.add(hueLabel, 1, 0);
+            root.add(saturationLabel, 1, 1);
+            root.add(brightnessLabel, 1, 2);
+            root.add(redLabel, 1, 3);
+            root.add(greenLabel, 1, 4);
+            root.add(blueLabel, 1, 5);
+            root.add(colorPatch, 2, 0, 1, 6);  // occupies 6 rows!
+            root.setStyle("-fx-padding:5px; -fx-border-color:darkblue; -fx-border-width:2px; -fx-background-color:#DDF");
+            
+            setColor(initialColor == null? Color.BLACK : initialColor);
+        }
+        
+        
+        public Color getColor() {
+            return currentColor;
+        }
+        
+        public void setColor(Color color) {
+            if (color == null)
+                return;
+            hueSlider.setValue(color.getHue());
+            brightnessSlider.setValue(color.getBrightness());
+            saturationSlider.setValue(color.getSaturation());
+            redSlider.setValue(color.getRed());
+            greenSlider.setValue(color.getGreen());
+            blueSlider.setValue(color.getBlue());
+            String colorString = String.format("#%02x%02x%02x", (int)(255*color.getRed()),
+                    (int)(255*color.getGreen()), (int)(255*color.getBlue()) );
+            colorPatch.setStyle("-fx-border-color:black; -fx-border-width:2px; -fx-background-color:" + colorString);
+            hueLabel.setText(String.format(" Hue = %1.3f", color.getHue()));
+            saturationLabel.setText(String.format(" Saturation = %1.3f", color.getSaturation()));
+            brightnessLabel.setText(String.format(" Brightness = %1.3f", color.getBrightness()));
+            redLabel.setText(String.format(" Red = %1.3f", color.getRed()));
+            greenLabel.setText(String.format(" Green = %1.3f", color.getGreen()));
+            blueLabel.setText(String.format(" Blue = %1.3f", color.getBlue()));
+            currentColor = color;
+        }
+        
+
+        private Label makeText(String message) {
+               
+            Label text = new Label(message);
+            text.setStyle("-fx-padding: 6px 10px 6px 10px; -fx-font-weight:bold");
+            return text;
+        }
+        
+        
+        private void newColor(Slider whichSlider) {
+            if ( ! whichSlider.isValueChanging() ) {
+                return; 
+            }
+            Color color;
+            if (whichSlider == redSlider || whichSlider == greenSlider || whichSlider == blueSlider) {
+                color = Color.color(redSlider.getValue(), greenSlider.getValue(), blueSlider.getValue());
+                hueSlider.setValue(color.getHue());
+                brightnessSlider.setValue(color.getBrightness());
+                saturationSlider.setValue(color.getSaturation());
+            }
+            else {
+                color = Color.hsb(hueSlider.getValue(), saturationSlider.getValue(), brightnessSlider.getValue());
+                redSlider.setValue(color.getRed());
+                greenSlider.setValue(color.getGreen());
+                blueSlider.setValue(color.getBlue());
+            }
+            currentColor = color;
+            String colorString = String.format("#%02x%02x%02x", (int)(255*color.getRed()),
+                    (int)(255*color.getGreen()), (int)(255*color.getBlue()) );
+            colorPatch.setStyle("-fx-border-color:black; -fx-border-width:2px; -fx-background-color:" + colorString);
+            hueLabel.setText(String.format(" Hue = %1.3f", color.getHue()));
+            saturationLabel.setText(String.format(" Saturation = %1.3f", color.getSaturation()));
+            brightnessLabel.setText(String.format(" Brightness = %1.3f", color.getBrightness()));
+            redLabel.setText(String.format(" Red = %1.3f", color.getRed()));
+            greenLabel.setText(String.format(" Green = %1.3f", color.getGreen()));
+            blueLabel.setText(String.format(" Blue = %1.3f", color.getBlue()));
+        }    
+
+
+    }  
+
+}
---
+++ c2w_BrowserWindow.java
@@ -0,0 +1,186 @@
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
+import javafx.concurrent.Worker;
+
+import java.util.ArrayList;
+
+
+
+class c2w_BrowserWindow extends Stage {
+
+    private c2w_WebBrowser c2w_owner;    
+    private WebEngine c2w_webEngine; 
+    private Menu c2w_windowMenu;     
+
+    c2w_BrowserWindow(c2w_WebBrowser c2w_browser, String c2w_initialURL) {
+        
+        c2w_owner = c2w_browser;
+        
+        
+        WebView webview = new WebView();
+        c2w_webEngine = webview.getEngine();
+        
+        
+        Label status = new Label("Status: Idle");  
+        status.setMaxWidth(Double.POSITIVE_INFINITY);
+        Label location = new Label("Location: (empty)");  
+        location.setMaxWidth(Double.POSITIVE_INFINITY);
+        TextField urlInput = new TextField();  
+        urlInput.setMaxWidth(600);
+        Button loadButton = new Button("Load"); 
+        loadButton.setOnAction( e -> doLoad(urlInput.getText()) );
+        loadButton.defaultButtonProperty().bind( urlInput.focusedProperty() );
+        Button cancelButton = new Button("Cancel");  
+        cancelButton.setDisable(true);  
+        
+        HBox loader = new HBox(8,new Label("URL:"), urlInput, loadButton, cancelButton);
+        HBox.setHgrow(urlInput, Priority.ALWAYS);
+        
+        VBox bottom = new VBox(10, location, status, loader);
+        bottom.setStyle("-fx-padding: 10px; -fx-border-color:black; -fx-border-width:3px 0 0 0");        
+        
+        BorderPane root = new BorderPane(webview);
+        root.setBottom(bottom);
+        root.setTop(makeMenuBar());
+        
+        setScene( new Scene(root) );
+        
+        
+        c2w_webEngine.locationProperty().addListener( (o,oldVal,newVal) -> {
+            if (newVal == null || newVal.equals("about:blank"))
+                location.setText("Location: (empty)");
+            else
+                location.setText("Location: " + newVal);
+        });
+        
+        c2w_webEngine.titleProperty().addListener( (o,oldVal,newVal) -> {
+            if (newVal == null)
+                setTitle("Untitled " + c2w_owner.c2w_getNextUntitledCount());
+            else
+                setTitle(newVal); 
+        });
+        
+        
+        c2w_webEngine.getLoadWorker().stateProperty().addListener( (o,oldVal,newVal) -> {
+            status.setText("Status: " + newVal);
+            switch (newVal) {
+            case READY:
+                status.setText("Status:  Idle.");
+                break;
+            case SCHEDULED:
+            case RUNNING:
+                status.setText("Status:  Loading a web page.");
+                break;
+            case SUCCEEDED:
+                status.setText("Status:  Web page has been successfully loaded.");
+                break;
+            case FAILED:
+                status.setText("Status:  Loading of the web page has failed.");
+                break;
+            case CANCELLED:
+                status.setText("Status:  Loading of the web page has been cancelled.");
+                break;
+            }
+            cancelButton.setDisable(newVal != Worker.State.RUNNING);
+        });
+        
+        cancelButton.setOnAction( e -> { 
+            if ( c2w_webEngine.getLoadWorker().getState() == Worker.State.RUNNING )
+                c2w_webEngine.getLoadWorker().cancel();
+            
+        });
+        
+        
+        c2w_webEngine.setOnAlert( evt -> SimpleDialogs.message(evt.getData(), "Alert from web page") );
+        c2w_webEngine.setPromptHandler( promptData -> 
+                    SimpleDialogs.prompt(promptData.getMessage(), 
+                                              "Query from web page", promptData.getDefaultValue()));
+        c2w_webEngine.setConfirmHandler( str -> 
+                    SimpleDialogs.confirm(str, "Confirmation Needed").equals("yes") );
+        
+        
+        if (c2w_initialURL != null) {
+               
+            doLoad(c2w_initialURL);
+        }
+        
+    } 
+    
+    
+    private void doLoad(String url) {
+        if (url == null || url.trim().length() == 0)
+            return;
+        url = url.trim();
+        if ( ! url.matches("^[a-zA-Z]+:.*")) {
+            url = "http://" + url;
+        }
+        System.out.println("Loading URL " + url);
+        c2w_webEngine.load(url);
+    }
+    
+    
+    private MenuBar makeMenuBar() {
+        MenuItem newWin = new MenuItem("New Window");
+        newWin.setOnAction( e -> c2w_owner.c2w_newBrowserWindow(null) );
+        MenuItem close = new MenuItem("Close Window");
+        close.setOnAction( e -> hide() );
+        MenuItem open = new MenuItem("Open URL in New Window...");
+        open.setOnAction( e -> {
+            String url = SimpleDialogs.prompt(
+                    "Enter the URL of the page that you want to open.", "Get URL");
+            if (url != null && url.trim().length() > 0)
+                c2w_owner.c2w_newBrowserWindow(url);
+        });
+        c2w_windowMenu = new Menu("Window");
+        c2w_windowMenu.getItems().addAll(newWin,close,open,new SeparatorMenuItem());
+        c2w_windowMenu.setOnShowing( e -> populateWindowMenu() );
+        MenuBar menubar = new MenuBar(c2w_windowMenu);
+        return menubar;
+    }
+    
+
+    private void populateWindowMenu() {
+        ArrayList<c2w_BrowserWindow> windows = c2w_owner.getOpenWindowList();
+        while (c2w_windowMenu.getItems().size() > 4) {
+               
+            c2w_windowMenu.getItems().remove(c2w_windowMenu.getItems().size() - 1);
+        }
+        if (windows.size() > 1) {
+            MenuItem item = new MenuItem("Close All and Exit");
+            item.setOnAction( e -> Platform.exit() );
+            c2w_windowMenu.getItems().add(item);
+            c2w_windowMenu.getItems().add( new SeparatorMenuItem() );
+        }
+        for (c2w_BrowserWindow window : windows) {
+            String title = window.getTitle(); 
+            if (title.length() > 60) {
+                title = title.substring(0,57) + ". . .";
+            }
+            MenuItem item = new MenuItem(title);
+            final c2w_BrowserWindow win = window; 
+            item.setOnAction( e -> win.requestFocus() );
+            c2w_windowMenu.getItems().add(item);
+            if (window == this) {
+                item.setDisable(true);
+            }
+        }
+    }
+    
+    
+} 
+
+
